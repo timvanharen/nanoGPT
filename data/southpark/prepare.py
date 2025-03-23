891a7f8410c1e14253863d6d30605ecb2dde13e6
timvanharen <timvanharen@live.com>
2025-03-23 15:20:01 +0100

    Meta file added, to support tokensize of GPT4

--- data/southpark/prepare.py
+++ data/southpark/prepare.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import pickle
 import tiktoken
 import numpy as np
 
@@ -18,12 +19,17 @@ train_ids = enc.encode_ordinary(train_data)
 val_ids = enc.encode_ordinary(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
+vocab_size = 200000
 
 # export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids = np.array(train_ids, dtype=np.uint32)
+val_ids = np.array(val_ids, dtype=np.uint32)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
+# save the meta information as well, to help us encode/decode later
+meta = {
+    'vocab_size': vocab_size,
+}
+with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
